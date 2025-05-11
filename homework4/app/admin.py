01551939dc87e006b66298434710212f10f4cf56
admin-yevhen.luzanov <admin-y.luzanov@metro.ua>
2025-05-11 21:46:10 +0300

    homework5

--- homework4/app/admin.py
+++ homework4/app/admin.py
@@ -1,5 +1,33 @@
 from django.contrib import admin
 from app.models.student import Student
+from app.models.department import Department
+from app.models.position import Position
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+    search_fields = ("name",)
+    list_filter = ("name",)
+    list_per_page = 10
+    ordering = ("id",)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "is_active",
+        "department",
+    )
+    search_fields = ("name",)
+    list_filter = ("is_active", "department")
+    list_per_page = 10
+    ordering = ("id",)
 
 
 @admin.register(Student)
